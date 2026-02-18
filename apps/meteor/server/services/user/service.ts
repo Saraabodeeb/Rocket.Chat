28c0eee3161c705fe8f6009d462482533b0554f8
sara abodeeb <saraabodeeb75@gmail.com>
2026-02-18 07:34:28 +0200

    refactor: finalize 2fa disable method

--- apps/meteor/server/services/user/service.ts
+++ apps/meteor/server/services/user/service.ts
@@ -3,18 +3,47 @@ import type { IUserService } from '@rocket.chat/core-services';
 import { Users } from '@rocket.chat/models';
 
 import { getMaxLoginTokens } from '../../lib/getMaxLoginTokens';
+import { TOTP } from '../../../app/2fa/server/lib/totp';
+import { notifyOnUserChange } from '../../../app/lib/server/lib/notifyListener';
 
 // TODO merge this service with Account service
 export class UserService extends ServiceClassInternal implements IUserService {
-	protected name = 'user';
+    protected name = 'user';
 
-	async ensureLoginTokensLimit(uid: string): Promise<void> {
-		const [{ tokens } = { tokens: [] }] = await Users.findAllResumeTokensByUserId(uid);
-		if (tokens.length < getMaxLoginTokens()) {
-			return;
-		}
+    async ensureLoginTokensLimit(uid: string): Promise<void> {
+        const [{ tokens } = { tokens: [] }] = await Users.findAllResumeTokensByUserId(uid);
+        if (tokens.length < getMaxLoginTokens()) {
+            return;
+        }
 
-		const oldestDate = tokens.reverse()[getMaxLoginTokens() - 1];
-		await Users.removeOlderResumeTokensByUserId(uid, oldestDate.when);
-	}
+        const oldestDate = tokens.reverse()[getMaxLoginTokens() - 1];
+        await Users.removeOlderResumeTokensByUserId(uid, oldestDate.when);
+    }
+
+    async disable2FA(userId: string, code: string): Promise<boolean> {
+        const user = await Users.findOneById(userId);
+        if (!user || !user.services?.totp?.enabled) {
+            return false;
+        }
+
+        const verified = await TOTP.verify({
+            secret: user.services.totp.secret,
+            token: code,
+            userId,
+            backupTokens: user.services.totp.hashedBackup,
+        });
+
+        if (!verified) {
+            return false;
+        }
+
+        const { modifiedCount } = await Users.disable2FAByUserId(userId);
+
+        if (modifiedCount) {
+            void notifyOnUserChange({ clientAction: 'updated', id: user._id, diff: { 'services.totp.enabled': false } });
+            return true;
+        }
+
+        return false;
+    }
 }
--- packages/core-services/src/types/IUserService.ts
+++ packages/core-services/src/types/IUserService.ts
@@ -1,3 +1,4 @@
 export interface IUserService {
 	ensureLoginTokensLimit(uid: string): Promise<void>;
+	disable2FA(userId: string, code: string): Promise<boolean>;
 }
